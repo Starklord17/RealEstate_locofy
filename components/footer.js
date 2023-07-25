d2a5cd45fcad53b01caa840b4771b6e2f3410922
MaxSM <maxi.sm117@gmail.com>
2023-07-25 18:16:24 -0300

    :zap: footer

--- components/footer.js
+++ components/footer.js
@@ -10,22 +10,22 @@ const Footer = () => {
           <div className="flex flex-row items-center justify-center gap-[8px]">
             <img className="w-11 h-11" alt="" src='/houseline.svg' />
             <div className="flex flex-col items-start justify-start">
-              <div className="relative leading-[24px] font-semibold">REIS</div>
-              <div className="relative text-sm leading-[16px] font-medium">
+              <div className="leading-[24px] font-semibold">REIS</div>
+              <div className="text-sm leading-[16px] font-medium">
                 Real State
               </div>
             </div>
           </div>
-          <div className="self-stretch h-[168px] flex flex-col items-start justify-start gap-[13px] text-left text-base text-gray-500 font-body-regular-400">
+          <div className="flex flex-col items-start justify-start gap-[13px] text-left text-base text-gray-500 font-body-regular-400">
             <div className="relative text-5xl leading-[32px] font-semibold text-gray-black">
               Contact Us
             </div>
-            <div className="relative leading-[24px]">Call : +123 400 123</div>
-            <div className="relative leading-[24px] flex items-end w-[312px]">
+            <div className="leading-[24px]">Call : +123 400 123</div>
+            <div className="leading-[24px] flex items-end">
               Praesent nulla massa, hendrerit vestibulum gravida in, feugiat
               auctor felis.
             </div>
-            <div className="relative leading-[24px] text-gray-700">
+            <div className="leading-[24px] text-gray-700">
               Email: example@mail.com
             </div>
           </div>
@@ -39,62 +39,62 @@ const Footer = () => {
             </div>
             <div className="rounded bg-primary-50 flex flex-col p-3.5 items-center justify-center">
               <img
-                className="relative w-5 h-5 overflow-hidden shrink-0"
+                className="w-5 h-5 overflow-hidden shrink-0"
                 alt=""
                 src='/social-media-logo1.svg'
               />
             </div>
             <div className="rounded bg-primary-50 flex flex-col p-3.5 items-center justify-center">
               <img
-                className="relative w-5 h-5 overflow-hidden shrink-0"
+                className="w-5 h-5 overflow-hidden shrink-0"
                 alt=""
                 src='/social-media-logo2.svg'
               />
             </div>
             <div className="rounded bg-primary-50 flex flex-col p-3.5 items-center justify-center">
               <img
-                className="relative w-5 h-5 overflow-hidden shrink-0"
+                className="w-5 h-5 overflow-hidden shrink-0"
                 alt=""
                 src='/social-media-logo3.svg'
               />
             </div>
             <div className="rounded bg-primary-50 flex flex-col p-3.5 items-center justify-center">
               <img
-                className="relative w-5 h-5 overflow-hidden shrink-0"
+                className="w-5 h-5 overflow-hidden shrink-0"
                 alt=""
                 src='/social-media-logo4.svg'
               />
             </div>
           </div>
         </div>
-        <div className="w-[173px] h-[248px] flex flex-col items-start justify-start gap-[32px]">
-          <div className="relative leading-[32px] font-semibold">Features</div>
+        <div className="w-[173px] flex flex-col items-start justify-start gap-[32px]">
+          <div className="leading-[32px] font-semibold">Features</div>
           <div className="self-stretch h-[184px] flex flex-col items-start justify-start gap-[16px] text-base text-gray-500">
-            <div className="relative leading-[24px]">Home</div>
-            <div className="relative leading-[24px]">Become a Host</div>
-            <div className="relative leading-[24px]">Pricing</div>
-            <div className="relative leading-[24px]">Blog</div>
-            <div className="relative leading-[24px]">Contact</div>
+            <div className="leading-[24px]">Home</div>
+            <div className="leading-[24px]">Become a Host</div>
+            <div className="leading-[24px]">Pricing</div>
+            <div className="leading-[24px]">Blog</div>
+            <div className="leading-[24px]">Contact</div>
           </div>
         </div>
-        <div className="w-[173px] h-[248px] flex flex-col items-start justify-start gap-[32px]">
-          <div className="relative leading-[32px] font-semibold">Company</div>
+        <div className="w-[173px] flex flex-col items-start justify-start gap-[32px]">
+          <div className="leading-[32px] font-semibold">Company</div>
           <div className="self-stretch h-[184px] flex flex-col items-start justify-start gap-[16px] text-base text-gray-500">
-            <div className="relative leading-[24px]">About Us</div>
-            <div className="relative leading-[24px]">Press</div>
-            <div className="relative leading-[24px]">Contact</div>
-            <div className="relative leading-[24px]">Careers</div>
-            <div className="relative leading-[24px]">Blog</div>
+            <div className="leading-[24px]">About Us</div>
+            <div className="leading-[24px]">Press</div>
+            <div className="leading-[24px]">Contact</div>
+            <div className="leading-[24px]">Careers</div>
+            <div className="leading-[24px]">Blog</div>
           </div>
         </div>
-        <div className="w-[203px] h-[168px] flex flex-col items-start justify-start gap-[32px]">
-          <div className="relative leading-[32px] font-semibold">
+        <div className="w-[203px] flex flex-col items-start justify-start gap-[32px]">
+          <div className="leading-[32px] font-semibold">
             Team and policies
           </div>
-          <div className="self-stretch h-[104px] flex flex-col items-start justify-start gap-[16px] text-base text-gray-500">
-            <div className="relative leading-[24px]">Terms of servies</div>
-            <div className="relative leading-[24px]">Privacy Policy</div>
-            <div className="relative leading-[24px]">Security</div>
+          <div className="h-[104px] flex flex-col items-start justify-start gap-[16px] text-base text-gray-500">
+            <div className="leading-[24px]">Terms of servies</div>
+            <div className="leading-[24px]">Privacy Policy</div>
+            <div className="leading-[24px]">Security</div>
           </div>
         </div>
       </footer>
